89b3a0aa9e65571d82960789b8a44a99dfa9c30c
ous <oussama.ghazouani@insat.ucar.tn>
2025-11-18 18:28:49 +0100

    d

---
+++ angular-practice/src/app/carte-visite/carte-visite.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carte-visite',
+  standalone: true,
+  templateUrl: './carte-visite.component.html',
+  styleUrls: ['./carte-visite.component.css']
+})
+export class CarteVisiteComponent {}
