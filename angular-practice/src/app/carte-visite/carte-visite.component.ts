46cdc6c687b20c1adb16ef76b20f1b0b46a90679
ous <oussama.ghazouani@insat.ucar.tn>
2025-11-18 19:50:48 +0100

    now the carte visite logic is done , added a form and all

--- angular-practice/src/app/carte-visite/carte-visite.component.ts
+++ angular-practice/src/app/carte-visite/carte-visite.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carte-visite',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './carte-visite.component.html',
   styleUrls: ['./carte-visite.component.css']
 })
-export class CarteVisiteComponent {}
+export class CarteVisiteComponent {
+  name = signal('john');
+  firstName = signal('doe');
+  job = signal('Student');
+  coverImage = signal('https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80');
+  profileImage = signal('https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80');
+  favoriteQuote = signal("We don't rise to the level of our expectations, we fall to the level of our training. - Archilochus");
+  workDescription = signal('I am a passionate web designer with an interest in creating visually stunning and user-friendly websites.');
+  workKeywords = signal('Web design, angular, HTML5, CSS3, NestJs...');
+
+  onCoverImageSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.convertToBase64(file).then(base64 => {
+        this.coverImage.set(base64);
+      });
+    }
+  }
+
+  onProfileImageSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.convertToBase64(file).then(base64 => {
+        this.profileImage.set(base64);
+      });
+    }
+  }
+
+  private convertToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = error => reject(error);
+    });
+  }
+}
