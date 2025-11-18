f85f264c520af77c2abe88264e325b7eed5e6ed3
ous <oussama.ghazouani@insat.ucar.tn>
2025-11-18 18:05:16 +0100

    color changes with text given

--- angular-practice/src/app/app.routes.ts
+++ angular-practice/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { ComponentTestComponent } from './component-test/component-test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: 'component-test', component: ComponentTestComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' }];
--- angular-practice/src/app/component-test/component-test.component.ts
+++ angular-practice/src/app/component-test/component-test.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-component-test',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './component-test.component.html',
   styleUrl: './component-test.component.css'
 })
 export class ComponentTestComponent {
+  color = signal('red');
 
 }
