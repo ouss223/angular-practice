c133aa8c51e690e859177fe2c4acf0042cf9f628
ous <oussama.ghazouani@insat.ucar.tn>
2025-11-18 17:50:14 +0100

    first test component

---
+++ angular-practice/src/app/component-test/component-test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-test',
+  imports: [],
+  templateUrl: './component-test.component.html',
+  styleUrl: './component-test.component.css'
+})
+export class ComponentTestComponent {
+
+}
